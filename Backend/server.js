9ce803681a2e6aa7bcee176e3bea58c6d54dc152
aashish-dev01 <aashish01.net@gmail.com>
2026-07-22 02:50:50 +0530

    adding front url

--- Backend/server.js
+++ Backend/server.js
@@ -12,7 +12,7 @@ connectDB();     // connect database
 const PORT = process.env.PORT || 3000; 
 const app = express(); 
 
-app.use(cors({ origin: ["http://localhost:5173", ], credentials: true})); // let origin use our server
+app.use(cors({ origin: ["http://localhost:5173", "https://notes-management-system-mocha.vercel.app/" ], credentials: true})); // let origin use our server
 app.use(express.json());  // parse json data
 app.use(express.urlencoded({ extended: true })); // parse html form data
 app.use(cookieParser());   // Cookies ko req.cookies me read karne ke liye.
