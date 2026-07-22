9600c85bd1000d3a18f5ddf53b09ed41e8d06d91
aashish-dev01 <aashish01.net@gmail.com>
2026-07-22 17:53:43 +0530

    cokkie issue solved

--- Backend/controllers/usercontroller.js
+++ Backend/controllers/usercontroller.js
@@ -46,7 +46,7 @@ export const registerUser = async (req, res) => {
 
             httpOnly: true,
             secure: true,
-            sameSite: "None",
+            sameSite: "none",
             maxAge: 7 * 24 * 60 * 60 * 1000
 
         });
@@ -124,6 +124,8 @@ export const loginUser = async (req, res) => {
         res.cookie("token", token, {
 
             httpOnly: true,
+             secure: true,
+            sameSite: "none",
 
             maxAge: 7 * 24 * 60 * 60 * 1000
 
